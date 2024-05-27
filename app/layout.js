38094cff34ee12e90d84f7987f4b166d566ab295
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-05-27 19:48:52 +0530

    Header section done

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,8 @@
-import { Inter } from "next/font/google";
+import { Inter_Tight } from "next/font/google";
 import "./globals.css";
+import Provider from "./Provider";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter_Tight({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +12,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Provider>
+        {children}
+        </Provider>
+        </body>
     </html>
   );
 }
